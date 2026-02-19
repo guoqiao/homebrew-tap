0195b68b4ca863f663475408b8f9c44cfede8041
guoqiao <guoqiao@gmail.com>
2026-02-20 11:50:32 +1300

    Fix merge conflict in mlx-audio-server formula

--- Formula/mlx-audio-server.rb
+++ Formula/mlx-audio-server.rb
@@ -5,12 +5,8 @@ class MlxAudioServer < Formula
   homepage "https://github.com/guoqiao/mlx-audio"
   license "MIT"
 
-  # stable version: brew install guoqiao/tap/mlx-audio-server
   url "https://github.com/guoqiao/mlx-audio.git", revision: "b017be1"
-  version "0.3.1-b017be1"
-
-  # HEAD version: brew install --HEAD guoqiao/tap/mlx-audio-server
-  head "https://github.com/guoqiao/mlx-audio.git", branch: "main"
+  version "0.3.2-b017be1"
 
   depends_on "ffmpeg"
   depends_on "python@3.12"
@@ -18,24 +14,16 @@ class MlxAudioServer < Formula
   def install
     virtualenv_create(libexec, "python3.12")
     system libexec/"bin/python", "-m", "ensurepip"
+    system libexec/"bin/python", "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"
     system libexec/"bin/python", "-m", "pip", "install", ".[server]"
 
-    # Link the entry points
-    # Expose with friendly names (dashes)
     bin.install_symlink libexec/"bin/mlx_audio.server" => "mlx-audio-server"
     bin.install_symlink libexec/"bin/mlx_audio.convert" => "mlx-audio-convert"
     bin.install_symlink libexec/"bin/mlx_audio.stt.generate" => "mlx-audio-stt-generate"
     bin.install_symlink libexec/"bin/mlx_audio.tts.generate" => "mlx-audio-tts-generate"
-
-    # Also expose original dotted names for compatibility
-    bin.install_symlink libexec/"bin/mlx_audio.server"
-    bin.install_symlink libexec/"bin/mlx_audio.convert"
-    bin.install_symlink libexec/"bin/mlx_audio.stt.generate"
-    bin.install_symlink libexec/"bin/mlx_audio.tts.generate"
   end
 
   def post_install
-    # Create directories for logs and state
     (var/"mlx-audio-server").mkpath
     (var/"log/mlx-audio-server").mkpath
   end
@@ -46,7 +34,7 @@ class MlxAudioServer < Formula
     keep_alive true
     working_dir var/"mlx-audio-server"
     log_path var/"log/mlx-audio-server/server.log"
-    error_log_path var/"log/mlx-audio-server/server.log"
+    error_log_path var/"log/mlx-audio-server/server.error.log"
     environment_variables PATH: std_service_path_env
   end
 
