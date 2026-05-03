61680fa00a740e9ef1555a20002a87a94de7b7f7
guoqiao <guoqiao@gmail.com>
2026-05-03 15:35:11 +1200

    fix github url

--- Casks/essential-fonts.rb
+++ Casks/essential-fonts.rb
@@ -2,6 +2,7 @@ cask "essential-fonts" do
   version :latest
   sha256 :no_check
 
+  url "https://github.com/guoqiao/homebrew-tap"
   name "Essential Coding Fonts"
   desc "Meta cask to install Fira Code, JetBrains Mono, and Source Code Pro"
   homepage "https://github.com/guoqiao/homebrew-tap"
